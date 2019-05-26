e0134b0187b4c6754dcd3cb049e940777d6483e1
MoonMan42 <miller42matt@gmail.com>
2019-05-26 16:51:18 -0400

    Upgraded to multipage format and removed the old signle page startup

---
+++ Notes/Notes/NoteEntry.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Notes.Models;
+using System;
+using System.IO;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntry : ContentPage
+    {
+        public NoteEntry()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // save
+                var Filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(Filename, note.Text);
+            } else
+            {
+                // update 
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        private async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+
+}
